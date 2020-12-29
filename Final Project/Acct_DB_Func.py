c7bb775186278223d804d6e70d85d5b1490b2f15
the-salty-austin <68510743+the-salty-austin@users.noreply.github.com>
2020-12-29 08:06:39 +0800

    Update Acct_DB_Func.py

--- Final Project/Acct_DB_Func.py
+++ Final Project/Acct_DB_Func.py
@@ -2,22 +2,10 @@ import sqlite3
 from datetime import datetime as dt
 import random as rdm
 
-conn = sqlite3.connect('Database\\book_keeping.sqlite')
-cur = conn.cursor()
-
 class Entry():
-    #def __init__(self, in_ex, description, ymd, price, account, record_time, sub_id):
-    #def __init__(self):
-        #self.conn = sqlite3.connect('Database\\book_keeping.sqlite')
-        #self.cur = self.conn.cursor()
-        #self.in_ex = in_ex  # income or expense?
-        #self.description = description  # str
-        #self.ymd = ymd  # Format: 2020-12-12
-        #self.price = price
-        #self.account = account  # cash / credit
-        #self.record_time = record_time  # str: YYYY-MM-DD HH:MM:SS.SSS
-        #self.sub_id = sub_id
-        #self.main_id = main_id
+    def __init__(self):
+        self.conn = sqlite3.connect('Database\\book_keeping.sqlite')
+        self.cur = self.conn.cursor()
 
     def parse_ymd(self, ymd):
         '''
@@ -27,19 +15,29 @@ class Entry():
         dt_obj = dt.strptime(ymd, '%Y-%m-%d')
         return dt_obj
 
-    def add_entry(self, in_ex, description, ymd, price, method, record_time, sub_id):
+    def add_entry(self, in_ex, description, cui_id, ymd, price, method, record_time, id):
         '''
         Takes 7 params: in_ex, description, ymd, price, method, record_time, sub_id
         '''
-        sqlstr = f'''
-        INSERT INTO {in_ex}
-                (description,ymd,price,method,record_time,SubCat_id)
-            VALUES
-                (?,?,?,?,?,?);
-        '''
-        cur.execute(sqlstr, (description, ymd, price,
-                             method, record_time, sub_id))
-        conn.commit()
+        if in_ex == 'Expense':
+            sqlstr = '''
+            INSERT INTO Expense
+                    (description,cuisine_id,ymd,price,method,record_time,SubCat_id)
+                VALUES
+                    (?,?,?,?,?,?,?);
+            '''
+            self.cur.execute(sqlstr, (description, cui_id, ymd, price,
+                                method, record_time, id))
+        elif in_ex == 'Income':
+            sqlstr = '''
+            INSERT INTO Income
+                    (description,cuisine_id,ymd,price,method,record_time,MainCat_id)
+                VALUES
+                    (?,?,?,?,?,?,?);
+            '''
+            self.cur.execute(sqlstr, (description, cui_id, ymd, price,
+                                method, record_time, id))
+        self.conn.commit()
 
     def delete_entry(self, id, in_ex):
         '''
@@ -47,8 +45,8 @@ class Entry():
         Deletes one entry from DB with specified id
         '''
         sqlstr = f'DELETE FROM {in_ex} WHERE id=?'
-        cur.execute(sqlstr, (id, ))
-        conn.commit()
+        self.cur.execute(sqlstr, (id, ))
+        self.conn.commit()
 
     def get_entry(self, id, in_ex):
         '''
@@ -56,10 +54,10 @@ class Entry():
         Returns a tuple containing 7 elements:
         id,description,ymd,price,method,record_time,SubCat_id
         '''
-        sqlstr = f'''SELECT id,description,ymd,price,method,record_time,SubCat_id
+        sqlstr = f'''SELECT id,description,cuisine_id,ymd,price,method,record_time,SubCat_id
                     FROM {in_ex} WHERE id=?'''
 
-        for entry in cur.execute(sqlstr, (id, )):
+        for entry in self.cur.execute(sqlstr, (id, )):
             search = entry  # returns a tuple
 
         return search
@@ -71,45 +69,113 @@ class Entry():
         # get old info
         old = self.get_entry(id, in_ex)
 
-        # to be connected to User Interface
-        new = input('description, ymd, price, method, subcat: ').split(',')
+        # to be DBconnected to User Interface
+        new = input('description, cuisine, ymd, price, method, subcat: ').split(',')
 
         # either inputs new data or keeps old data
         description = new[0] if len(new[0]) > 0 else old[1]
-        ymd = new[1] if len(new[1]) > 0 else old[2]
-        price = int(new[2]) if len(new[2]) > 0 else old[3]
-        method = int(new[3]) if len(new[3]) > 0 else old[4]
-        sub_id = int(new[4]) if len(new[4]) > 0 else old[6]
+        cui_id = int(new[1]) if len(new[1]) > 0 else old[2]
+        ymd = new[2] if len(new[2]) > 0 else old[3]
+        price = int(new[3]) if len(new[3]) > 0 else old[4]
+        method = int(new[4]) if len(new[4]) > 0 else old[5]
+        sub_id = int(new[5]) if len(new[5]) > 0 else old[7]
 
         sqlstr = f'''UPDATE {in_ex} SET
-                    (description,ymd,price,method,SubCat_id)=(?,?,?,?,?)
+                    (description,cuisine_id,ymd,price,method,SubCat_id)=(?,?,?,?,?,?)
                     WHERE id=?'''
 
-        cur.execute(sqlstr, (description, ymd, price,
+        self.cur.execute(sqlstr, (description, cui_id, ymd, price,
                              method, sub_id, old[0]))
-        conn.commit()
+        self.conn.commit()
+
+    def update_budget(self, year, month, budget):
+        '''
+        Takes 2 params: year, month
+        '''
+
+        sqlstr = f'''UPDATE Budget SET
+                    amt=? WHERE year=? AND month=?'''
+
+        self.cur.execute(sqlstr, (budget,year,month))
+        self.conn.commit()
+
+    def output_expense_categories(self):
+        cat_dict = {}
+
+        for i in range(1,9):
+            sqlstr = f'''SELECT MainCat.MainCat, SubCat.SubCat, SubCat.id
+                        FROM SubCat
+                        JOIN MainCat ON SubCat.MainCat_id = MainCat.id
+                        WHERE MainCat.id = {i}'''
+
+            for output in self.cur.execute(sqlstr):
+                #output[0]: MainCat /  output[1]: SubCat
+                maincat, subcat, subcat_id = output[0], output[1], output[2]
+                if maincat not in cat_dict.keys():
+                    cat_dict[maincat] = [(subcat, subcat_id)]
+                else:
+                    cat_dict[maincat].append((subcat, subcat_id))
+        
+        return cat_dict
+
+    def output_income_categories(self):
+        cat_lst = []
+
+        for i in range(9,12):
+            sqlstr = f'''SELECT MainCat.MainCat, MainCat.id
+                        FROM MainCat
+                        WHERE MainCat.id = {i}'''
+
+            for output in self.cur.execute(sqlstr):
+                cat_lst.append((output[0],output[1]))
+        print(cat_lst)
+        return cat_lst
 
 
-class Expense(Entry):
-    pass
+    def output_cuisine_categories(self):
+        cui_lst = []
+
+        for i in range(1,16):
+            sqlstr = f'''SELECT id, cuisine
+                        FROM Cuisine
+                        WHERE Cuisine.id = {i}'''
+
+            for output in self.cur.execute(sqlstr):
+                cui_lst.append((output[0],output[1]))
+        
+        return cui_lst
+
+    def output_paymethod_categories(self):
+        pay_lst = []
+
+        for i in range(1,5):
+            sqlstr = f'''SELECT Paymethod, id FROM Method
+                        WHERE Method.id = {i}'''
+
+            for output in self.cur.execute(sqlstr):
+                pay_lst.append((output[0],output[1]))
+        
+        return pay_lst
+
+    
 
 
-class Income(Entry):
-    pass
+class Recommendation():
+  
+    def get_restaurants(id=1):
+        conn = sqlite3.connect('Database\\scores.sqlite')
+        cur = conn.cursor()
+        
+        out = []
+        
+        sqlstr = f'''SELECT Data.name, Data.score, Data.price, Data.geo, Cuisine
+                     FROM Data JOIN Cuisine ON Cuisine.id = Data.cuisineID
+                     WHERE Cuisine.id = {id}
+                     ORDER BY Data.score DESC LIMIT 20'''
+        for row in cur.execute(sqlstr):
+            out.append(row)
 
-
-class Insights():
-    def get_sum(self, timespan, category):
-        pass
-
-    def get_avg(self, timespan, category):
-        pass
-
-    def get_percentage(self, what_kind):
-        pass
-
-    def set_budget_constraint(self, timespan):
-        pass
+        return out
 
 
 class Visualization():
@@ -127,7 +193,7 @@ class Visualization():
 def select_main_cat():
     maincats = {}
     sqlstr = 'SELECT id, MainCat FROM MainCat'
-    for cat in cur.execute(sqlstr):
+    for cat in self.cur.execute(sqlstr):
         cat_id = cat[0]
         cat_name = cat[1]
         print(cat_id, cat_name)
@@ -144,7 +210,7 @@ def select_sub_cat(chosen_main_id):
     subcats = {}
     sqlstr = 'SELECT id, SubCat, MainCat_id FROM SubCat'
 
-    for subcat in cur.execute(sqlstr):
+    for subcat in self.cur.execute(sqlstr):
         maincat_id = subcat[2]
         if chosen_main_id == maincat_id:
             subcat_id = subcat[0]
@@ -160,31 +226,34 @@ def select_sub_cat(chosen_main_id):
 
 
 # Below only for testing: to add new record entries...
-'''
-test = Entry()
 
-num = 0
-for i in range(1000):
+# test = Entry()
 
-    num += 1
-    description = 'IC-' + str(num)
-    month = rdm.randint(1,12)
-    month = str(month) if month >= 10 else '0'+str(month)
-    day = rdm.randint(1,28)
-    day = str(day) if day >= 10 else '0'+str(day)
-    date = '2020-' + month + '-' + day
-    price = rdm.randint(10,10000)
-    method = rdm.randint(1,4)
-    timenow = dt.strptime(dt.now().isoformat(), '%Y-%m-%dT%H:%M:%S.%f')  # YYYY-MM-DD HH:MM:SS.SSS
-    while True:
-        cat = rdm.randint(1,27)
-        if 12<=cat<=14:
-            continue
-        else:
-            break
+# num = 0
+# for i in range(150):
 
-    test.add_entry('Income',description,date,price,method,timenow,cat)
-'''
+#     num += 1
+#     description = 'EX-' + str(num)
+#     month = rdm.randint(1,12)
+#     month = str(month) if month >= 10 else '0'+str(month)
+#     day = rdm.randint(1,28)
+#     day = str(day) if day >= 10 else '0'+str(day)
+#     date = '2020-' + month + '-' + day
+#     price = rdm.randint(10,10000)
+#     method = rdm.randint(1,4)
+#     timenow = dt.strptime(dt.now().isoformat(), '%Y-%m-%dT%H:%M:%S.%f')  # YYYY-MM-DD HH:MM:SS.SSS
+#     cui_id = rdm.randint(1,15)
+#     #cui_id = 16
+#     # while True:
+#     #     cat = rdm.randint(1,23)
+#     #     if 12<=cat<=14:
+#     #         continue
+#     #     else:
+#     #         break
+#     cat = rdm.randint(1,18)
+
+#     test.add_entry('Expense',description,cui_id,date,price,method,timenow,cat)
+
 # should return a tuple
 #x = test.get_entry(1, 'Expense')
 #print(x)
@@ -194,4 +263,10 @@ for i in range(1000):
 #test.add_entry('Income','test-100','1980-01-01',25,1,'N\\A',5)
 
 #test.delete_entry(1, 'Income')
-cur.close()
+# test = Entry()
+# cats = test.output_paymethod_categories()
+# print(cats)
+# #test.update_budget(2020,12,10300)
+
+# test2 = Recommendation()
+# test2.get_restaurants(1)
